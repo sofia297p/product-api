3cf8745bbb40565f1e7e15de9d9916984990ac75
sofia297p <sofia297484@gmail.com>
2025-09-13 17:06:05 +0300

    Added find by id, remove by id, remove all, amount, most expensive, most cheap, median

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 
 @Injectable()
 export class ProductsService {
+  constructor(private prisma: PrismaService){}
   
+  private async sortAscByPrice() {
+
+    return await this.prisma.product.findMany({orderBy:{price:"asc"}});
+
+  }
+
+  async findById(id: number) {
+    const product = await this.prisma.product.findUnique({where:{id}});
+
+    if(!product){
+      throw new NotFoundException("Product not found");
+    }
+
+    return product;
+  }
+
+
+  async removeById(id: number) {
+    await this.findById(id);
+    
+    await this.prisma.product.delete({where:{id}});
+
+  }
+
+ 
+  async removeAll() {
+
+    await this.prisma.product.deleteMany();
+
+  }
+
+  async getAmount() {
+
+    return await this.prisma.product.count();
+
+  }
+
+  async getTheMostExpensiveProduct() {
+
+    const sortedProducts = await this.sortAscByPrice();
+
+    return sortedProducts[sortedProducts.length - 1];
+
+  }
+
+  async getTheCheapestProduct() {
+
+    const sortedProducts = await this.sortAscByPrice();
+
+    return sortedProducts[0];
+
+  }
+
+
+  async getMedianProduct() {
+
+    const amountOfProducts = await this.getAmount();
+
+    if(amountOfProducts % 2 === 0){
+
+      const sortedProducts = await this.sortAscByPrice();
+
+      const medianIndex = Math.ceil(sortedProducts.length / 2);
+
+      return sortedProducts[medianIndex];
+
+    }
+    throw new BadRequestException("Incorect number of products.")
+
+  }
 }
