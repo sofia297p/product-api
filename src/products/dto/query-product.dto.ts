c032699b061c1ec1c9fb4be3eed0c8c5c9d4ea6f
sofia297p <sofia297484@gmail.com>
2025-09-13 19:25:59 +0300

    Created dtos

---
+++ src/products/dto/query-product.dto.ts
@@ -0,0 +1,26 @@
+import { IsEnum, IsInt, IsOptional, Min } from 'class-validator';
+import { SortDirection } from 'src/common/enums/sort-direction.enum';
+import { ProductSortField } from 'src/common/enums/product-sort-field.enum';
+import { Transform } from 'class-transformer';
+
+export class QueryProductsDto {
+  @IsInt()
+  @Min(1)
+  @IsOptional()
+  @Transform(({value}) => Number(value))
+  pageIndex:number = 1;
+
+  @IsInt()
+  @Min(1)
+  @IsOptional()
+  @Transform(({value}) => Number(value))
+  pageSize:number = 10;
+
+  @IsEnum(ProductSortField)
+  @IsOptional()
+  sortField?:ProductSortField
+
+  @IsEnum(SortDirection)
+  @IsOptional()
+  sort?:SortDirection
+}
