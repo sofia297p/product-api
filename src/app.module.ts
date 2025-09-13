730d8d9e31195e3595c5dd4609d2414dd7bc2a31
sofia297p <sofia297484@gmail.com>
2025-09-13 16:24:19 +0300

    Added initial product module service controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule],
 })
 export class AppModule {}
