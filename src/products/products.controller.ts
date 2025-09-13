d4f40e6dd42341bf17216413dbbe88a0a676fb28
sofia297p <sofia297484@gmail.com>
2025-09-13 20:23:34 +0300

    Implemented controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,56 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { QueryProductsDto } from './dto/query-product.dto';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  
+  @Post()
+  async create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
+  }
+
+  @Get()
+  async getListOfProducts(@Query() query: QueryProductsDto) {
+    return this.productsService.getListOfProducts(query);
+  }
+
+  @Get("expensivest")
+  async getTheMostExpensiveProduct() {
+    return this.productsService.getTheMostExpensiveProduct();
+  }
+
+  @Get("cheapest")
+  async getTheCheapestProduct() {
+    return this.productsService.getTheCheapestProduct();
+  }
+
+  @Get("meadian")
+  async getMedianProduct() {
+    return this.productsService.getMedianProduct();
+  }
+
+  @Get("count")
+  async getAmount() {
+    return this.productsService.getAmount();
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    return this.productsService.findById(+id);
+  }
+
+
+  @Delete(':id')
+  async removeById(@Param('id') id: string) {
+    return this.productsService.removeById(+id);
+  }
+
+  @Delete()
+  async removeAll() {
+    return this.productsService.removeAll();
+  }
+
 }
