a54b60cff82a77f68203a5da03ae23d2b0b64d3e
sofia297p <sofia297484@gmail.com>
2025-09-13 20:57:48 +0300

    Added swager + validation for id

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  const config = new DocumentBuilder()
+    .setTitle("Products")
+    .setDescription("The products API description")
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,54 +1,68 @@
-import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { QueryProductsDto } from './dto/query-product.dto';
+import { ApiExtraModels, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags("Products")
+@ApiExtraModels(QueryProductsDto)
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @ApiOperation({ summary: "Create a new product" })
   async create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
+  @ApiOperation({ summary: "Get a list of products with pagination and sorting" })
   async getListOfProducts(@Query() query: QueryProductsDto) {
     return this.productsService.getListOfProducts(query);
   }
 
   @Get("expensivest")
+  @ApiOperation({ summary: 'Get the most expensive product' })
   async getTheMostExpensiveProduct() {
     return this.productsService.getTheMostExpensiveProduct();
   }
 
   @Get("cheapest")
+  @ApiOperation({ summary: "Get the cheapest product" })
   async getTheCheapestProduct() {
     return this.productsService.getTheCheapestProduct();
   }
 
   @Get("meadian")
+  @ApiOperation({ summary: "Get median product" })
   async getMedianProduct() {
     return this.productsService.getMedianProduct();
   }
 
   @Get("count")
+  @ApiOperation({ summary: "Get the total number of products" })
   async getAmount() {
     return this.productsService.getAmount();
   }
 
   @Get(':id')
-  async findById(@Param('id') id: string) {
-    return this.productsService.findById(+id);
+  @ApiOperation({ summary: "Get product by ID" })
+  async findById(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.findById(id);
   }
 
 
   @Delete(':id')
-  async removeById(@Param('id') id: string) {
+  @ApiOperation({ summary: 'Delete a product by ID' })
+  @HttpCode(204)
+  async removeById(@Param('id', ParseIntPipe) id: number) {
     return this.productsService.removeById(+id);
   }
 
   @Delete()
+  @HttpCode(204)
+  @ApiOperation({ summary: 'Delete all products' })
   async removeAll() {
     return this.productsService.removeAll();
   }
